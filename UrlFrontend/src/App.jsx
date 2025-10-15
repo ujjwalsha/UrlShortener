35c3c5a1d319320548d04b8f25b3855a0e97d787
Ujjwal Kumar <2825794@tcs.com>
2025-10-15 13:44:14 +0530

    url short feature, copy feature

--- UrlFrontend/src/App.jsx
+++ UrlFrontend/src/App.jsx
@@ -1,33 +1,110 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './Components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [originalUrl, setOriginalUrl] = useState('');
+  const [trigger, setTrigger] = useState(false);
+  const [shorturl, setShortUrl] = useState('');
+  const [copyStatus, setCopystatus] = useState('')
+
+  const shortUrl = async () => {
+
+    console.log(originalUrl);
+    
+
+    if(!originalUrl) return;
+
+    try{
+      const response = await fetch('http://localhost:8081/api/create',
+        { method : "POST",
+          headers : {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({originalUrl:originalUrl}),
+        });
+  
+        const data = await response.json();
+        console.log(data);
+        
+
+        const url = data.shortUrl;
+        setShortUrl(`http://localhost:8081/api/s/${url}`);
+        
+    
+    }
+    catch(error)
+    {
+      console.log("something wrong");
+      
+    }
+  }
+
+  const submitHandle = (e) =>{
+      e.preventDefault();
+     shortUrl();
+    console.log(originalUrl);
+      
+  }
+
+  const handleChange = (e) =>{
+      setOriginalUrl(e.target.value);
+  }
+
+  const handleCopyclick = async () =>{
+      try{
+        await navigator.clipboard.writeText(shorturl);
+        setCopystatus("copied!")
+        console.log(shorturl);
+        
+        setTimeout(() => {
+          setCopystatus('')
+        }, 2000);
+      }
+      catch(error)
+      {
+        console.log("something wrong in copy");
+        
+      }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <NavBar/>
+      <div className='grid justify-center gap-10 items-center p-20'>
+          <div className='search-bar'>
+              <input type="text" 
+                className='text-xl font-semibold w-xl p-2 border'
+                placeholder='Enter your Url'
+                value={originalUrl}
+                onChange={handleChange}
+              />
+              <button 
+              className='text-xl font-semibold border bg-blue-900 p-2 hover:bg-blue-950 cursor-pointer'
+              onClick={submitHandle}
+              >Submit</button>
+          </div>
+
+          <div className='shorturl-container border grid  w-2xl h-xl'>
+
+            <div className='flex justify-end'>
+                <button 
+                    className='text-[1em] p-2 cursor-pointer bg-gray-900 '
+                    onClick={handleCopyclick}
+                  >
+                    {copyStatus ||'Copy'}
+                  </button>
+            </div>
+            
+              <p className='p-10 font-semibold'>{shorturl || "url here"}</p>
+
+          </div>
+
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ UrlFrontend/src/Components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className='navbar-content'>
+        <h1 className='font-bold text-2xl '>URL-SHORTENER</h1>
+    </div>
+  )
+}
